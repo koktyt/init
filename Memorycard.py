c05283cb6e2aa19e2116b0a098fae1a192abe8d9
koktyt <92942258+koktyt@users.noreply.github.com>
2021-10-29 17:09:35 +0300

    Update Memorycard.py

--- Memorycard.py
+++ Memorycard.py
@@ -1,46 +1,62 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QRadioButton, QPushButton, QLabel)
+from PyQt5.QtWidgets import (
+        QApplication, QWidget, 
+        QHBoxLayout, QVBoxLayout, 
+        QGroupBox, QButtonGroup, QRadioButton,  
+        QPushButton, QLabel)
  
 app = QApplication([])
  
-window = QWidget()
-window.setWindowTitle('Memo Card')
+btn_OK = QPushButton('Ответить') 
+lb_Question = QLabel('Самый сложный вопрос в мире!') 
  
-'''Интерфейс приложения Memory Card'''
-btn_OK = QPushButton('Ответить') # кнопка ответа
-lb_Question = QLabel('В каком году была основана Москва?') # текст вопроса
+RadioGroupBox = QGroupBox("Варианты ответов")
  
-RadioGroupBox = QGroupBox("Варианты ответов") # группа на экране для переключателей с ответами
-rbtn_1 = QRadioButton('1147')
-rbtn_2 = QRadioButton('1242')
-rbtn_3 = QRadioButton('1861')
-rbtn_4 = QRadioButton('1943')
+rbtn_1 = QRadioButton('Вариант 1')
+rbtn_2 = QRadioButton('Вариант 2')
+rbtn_3 = QRadioButton('Вариант 3')
+rbtn_4 = QRadioButton('Вариант 4')
+ 
+RadioGroup = QButtonGroup() 
+RadioGroup.addButton(rbtn_1)
+RadioGroup.addButton(rbtn_2)
+RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
  
 layout_ans1 = QHBoxLayout()   
-layout_ans2 = QVBoxLayout() # вертикальные будут внутри горизонтального
+layout_ans2 = QVBoxLayout()
 layout_ans3 = QVBoxLayout()
-layout_ans2.addWidget(rbtn_1) # два ответа в первый столбец
+layout_ans2.addWidget(rbtn_1) 
 layout_ans2.addWidget(rbtn_2)
-layout_ans3.addWidget(rbtn_3) # два ответа во второй столбец
+layout_ans3.addWidget(rbtn_3) 
 layout_ans3.addWidget(rbtn_4)
  
 layout_ans1.addLayout(layout_ans2)
-layout_ans1.addLayout(layout_ans3) # разместили столбцы в одной строке
+layout_ans1.addLayout(layout_ans3)
  
-RadioGroupBox.setLayout(layout_ans1) # готова "панель" с вариантами ответов 
+RadioGroupBox.setLayout(layout_ans1)
  
-layout_line1 = QHBoxLayout() # вопрос
-layout_line2 = QHBoxLayout() # варианты ответов или результат теста
-layout_line3 = QHBoxLayout() # кнопка "Ответить"
+AnsGroupBox = QGroupBox("Результат теста")
+lb_Result = QLabel('прав ты или нет?') 
+lb_Correct = QLabel('ответ будет тут!')
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result, alignment=(Qt.AlignLeft | Qt.AlignTop))
+layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter, stretch=2)
+AnsGroupBox.setLayout(layout_res)
+ 
+layout_line1 = QHBoxLayout() 
+layout_line2 = QHBoxLayout() 
+layout_line3 = QHBoxLayout() 
  
 layout_line1.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
-layout_line2.addWidget(RadioGroupBox)
+layout_line2.addWidget(RadioGroupBox)   
+layout_line2.addWidget(AnsGroupBox)  
+AnsGroupBox.hide()
  
 layout_line3.addStretch(1)
 layout_line3.addWidget(btn_OK, stretch=2) # кнопка должна быть большой
 layout_line3.addStretch(1)
  
-# Теперь созданные строки разместим друг под другой:
 layout_card = QVBoxLayout()
  
 layout_card.addLayout(layout_line1, stretch=2)
@@ -50,6 +66,39 @@ layout_card.addLayout(layout_line3, stretch=1)
 layout_card.addStretch(1)
 layout_card.setSpacing(5) # пробелы между содержимым
  
+# ----------------------------------------------------------
+# Виджеты и макеты созданы, далее - функции:
+# ----------------------------------------------------------
+ 
+def show_result():
+    ''' показать панель ответов '''
+    RadioGroupBox.hide()
+    AnsGroupBox.show()
+    btn_OK.setText('Следующий вопрос')
+ 
+def show_question():
+    ''' показать панель вопросов '''
+    RadioGroupBox.show()
+    AnsGroupBox.hide()
+    btn_OK.setText('Ответить')
+    RadioGroup.setExclusive(False) # сняли ограничения, чтобы можно было сбросить выбор радиокнопки
+    rbtn_1.setChecked(False)
+    rbtn_2.setChecked(False)
+    rbtn_3.setChecked(False)
+    rbtn_4.setChecked(False)
+    RadioGroup.setExclusive(True) # вернули ограничения, теперь только одна радиокнопка может быть выбрана
+ 
+def test():
+    ''' временная функция, которая позволяет нажатием на кнопку вызывать по очереди
+    show_result() либо show_question() '''
+    if 'Ответить' == btn_OK.text():
+        show_result()
+    else:
+        show_question()
+ 
+window = QWidget()
 window.setLayout(layout_card)
+window.setWindowTitle('Memo Card')
+btn_OK.clicked.connect(test) # проверяем, что панель ответов показывается при нажатии на кнопку
 window.show()
 app.exec()
